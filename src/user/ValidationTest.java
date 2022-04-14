1f18867f25df9347db6ef9d6c65863218216202f
JuanGomezGlb <44448286+JuanGomezGlb@users.noreply.github.com>
2022-04-14 17:22:14 -0300

    Update ValidationTest.java

--- src/user/ValidationTest.java
+++ src/user/ValidationTest.java
@@ -9,8 +9,8 @@ import junit.framework.Assert;
 public class ValidationTest  {
 	@Test
     public void myTest1() {
-		validator v2=new validator();
-		boolean val=v2.Validate("Surabhi", "Infy123");
+		validator v3=new validator();
+		boolean val=v3.Validate("Surabhi", "Infy123");
 		Assert.assertEquals(true, val);
 	}
 	
