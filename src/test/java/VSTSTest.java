206b66e78db67cac2a2678844f93f9ca16c5bc72
JuanGomezGlb <44448286+JuanGomezGlb@users.noreply.github.com>
2022-04-14 17:04:34 -0300

    Update VSTSTest.java

--- src/test/java/VSTSTest.java
+++ src/test/java/VSTSTest.java
@@ -8,8 +8,8 @@ import user.validator;
 public class VSTSTest {
 	@Test
     public void myTest1() {
-		validator v1=new validator();
-		boolean val=v1.Validate("Surabhi", "Infy123");
+		validator v2=new validator();
+		boolean val=v2.Validate("Surabhi", "Infy123");
 		Assert.assertEquals(true, val);
 	}
 }
